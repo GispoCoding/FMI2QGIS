5955d77548d2f0ddaf712c3029a2e43632a9849e
Mikael Vaaltola <mvaaltola@gmail.com>
2020-10-21 16:25:05 +0300

    Update dummy test

--- FMI2QGIS/test/test_rastercreation.py
+++ FMI2QGIS/test/test_rastercreation.py
@@ -5,12 +5,13 @@ from qgis.core import QgsRasterLayer, QgsVectorLayer
 def test_netcdf_rasterlayer_creation():
     # setup
     netcdf_path = Path('/home/mikael/git/FMI2QGIS/FMI2QGIS/test/test_data/test.nc')
+    uri = f'NETCDF:"{netcdf_path}":time_bounds_h'
     layer_name = 'testlayer'
     provider_key = 'gdal'
     # opts: QgsRasterLayer.LayerOptions() = {}
 
     # test
-    layer = QgsRasterLayer(str(netcdf_path), layer_name, provider_key)
+    layer = QgsRasterLayer(str(netcdf_path), layer_name) # provider_key)
 
     # assertion
     print(layer.isValid())
