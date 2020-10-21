f647ee5e677572ae47512887ab828d2376a800d2
Mikael Vaaltola <mvaaltola@gmail.com>
2020-10-21 15:10:29 +0300

    Add dummy tests

---
+++ FMI2QGIS/test/test_download.py
@@ -0,0 +1,7 @@
+
+
+def test_netcdf_download():
+    pass
+
+def test_wfs_download():
+    pass
---
+++ FMI2QGIS/test/test_rastercreation.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from qgis.core import QgsRasterLayer, QgsVectorLayer
+
+
+def test_netcdf_rasterlayer_creation():
+    # setup
+    netcdf_path = Path('/home/mikael/git/FMI2QGIS/FMI2QGIS/test/test_data/test.nc')
+    layer_name = 'testlayer'
+    provider_key = 'gdal'
+    # opts: QgsRasterLayer.LayerOptions() = {}
+
+    # test
+    layer = QgsRasterLayer(str(netcdf_path), layer_name, provider_key)
+
+    # assertion
+    print(layer.isValid())
+
+def test_netcdf_vectorlayer_creation():
+    # setup
+    netcdf_path = '/home/mikael/git/FMI2QGIS/FMI2QGIS/test/test_data/test.nc'
+    layer_name = 'testlayer'
+    provider_key = 'ogr'
+    opts: QgsVectorLayer.LayerOptions() = {}
+
+    # test
+    layer = QgsVectorLayer(netcdf_path, layer_name, provider_key)
+
+    # assertion
+    print(layer.isValid())
