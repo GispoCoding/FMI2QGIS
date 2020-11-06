6ec26cd8ca5ade7999b06c5a339ba386ac6d0b18
Joonalai <joona@gispo.fi>
2020-11-06 10:46:04 +0200

    #18: Use initially general stored query solution in UI

--- FMI2QGIS/ui/main_dialog.py
+++ FMI2QGIS/ui/main_dialog.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 from PyQt5.QtWidgets import QDialog, QProgressBar
 from qgis.core import (QgsCoordinateReferenceSystem, QgsApplication, QgsProcessingAlgRunnerTask, QgsProcessingContext,
@@ -9,7 +9,10 @@ from qgis.gui import QgsExtentGroupBox, QgisInterface, QgsMapCanvas
 
 from ..core.processing.algorithms import FmiEnfuserLoaderAlg
 from ..core.processing.provider import Fmi2QgisProcessingProvider
+from ..core.processing.raster_loader import RasterLoader
 from ..core.products.enfuser import EnfuserNetcdfLoader
+from ..core.wfs import StoredQueryFactory, StoredQuery, Parameter
+from ..definitions.configurable_settings import Settings
 from ..qgis_plugin_tools.tools.custom_logging import bar_msg
 from ..qgis_plugin_tools.tools.i18n import tr
 from ..qgis_plugin_tools.tools.logger_processing import LoggerProcessingFeedBack
@@ -47,7 +50,47 @@ class MainDialog(QDialog, FORM_CLASS):
 
         self.responsive_items = {self.btn_load}
 
+        self.task = None
+        self.sq_factory = StoredQueryFactory(Settings.FMI_WFS_URL.get(), Settings.FMI_WFS_VERSION.get())
+
+        # TODO: do this only on demand when refreshing
+        self.stored_queries: List[StoredQuery] = self.sq_factory.list_queries()
+
     def __load_clicked(self):
+        enfuser_id = 'fmi::forecast::enfuser::airquality::helsinki-metropolitan::grid'
+        sq: StoredQuery = list(filter(lambda q: q.id == enfuser_id, self.stored_queries))[0]
+        # TODO: do expanding on demand with button and build parameters dynamically based on sq.parameters
+        self.sq_factory.expand(sq)
+
+        sq.parameters['starttime'].value = self.dt_edit_start.dateTime().toPyDateTime()
+        sq.parameters['endtime'].value = self.dt_edit_end.dateTime().toPyDateTime()
+        sq.parameters['bbox'].value = self.extent_group_box_bbox.outputExtent()
+
+        # TODO: as said, build this dynamically based on sq.parameters
+        variables = []
+        if self.chk_box_aqi.isChecked():
+            variables.append('AQIndex')
+        if self.chk_box_pm25.isChecked():
+            variables.append('PM25Concentration')
+        if self.chk_box_pm10.isChecked():
+            variables.append('PM10Concentration')
+        if self.chk_box_no2.isChecked():
+            variables.append('NO2Concentration')
+        if self.chk_box_o3.isChecked():
+            variables.append('O3Concentration')
+        sq.parameters['param'].value = variables
+
+        output_path = Path(self.output_dir_select_btn.filePath())
+        self.task = RasterLoader('enfuser', output_path, Settings.FMI_DOWNLOAD_URL.get(), sq)
+
+        # noinspection PyUnresolvedReferences
+        self.task.progressChanged.connect(lambda: self.progress_bar.setValue(self.task.progress()))
+        # noinspection PyArgumentList
+        QgsApplication.taskManager().addTask(self.task)
+        self._disable_ui()
+
+    def __load_clicked_old(self):
+        # TODO: Remove
         params = {
             FmiEnfuserLoaderAlg.AQI: (self.chk_box_aqi.isChecked()),
             FmiEnfuserLoaderAlg.PM25: (self.chk_box_pm25.isChecked()),
@@ -59,24 +102,20 @@ class MainDialog(QDialog, FORM_CLASS):
             FmiEnfuserLoaderAlg.EXTENT: (self.extent_group_box_bbox.outputExtent()),
             FmiEnfuserLoaderAlg.OUT_DIR: self.output_dir_select_btn.filePath()
         }
-
         # noinspection PyArgumentList
         alg = QgsApplication.processingRegistry().algorithmById(
             f'{Fmi2QgisProcessingProvider.ID}:{FmiEnfuserLoaderAlg.ID}')
-
         task = QgsProcessingAlgRunnerTask(alg, params, self.context, self.feedback)
-
         # noinspection PyUnresolvedReferences
         task.executed.connect(self._alg_completed)
-
         # noinspection PyUnresolvedReferences
         task.progressChanged.connect(lambda: self.progress_bar.setValue(task.progress()))
-
         # noinspection PyArgumentList
         QgsApplication.taskManager().addTask(task)
         self._disable_ui()
 
     def _alg_completed(self, succesful: bool, results: Dict[str, any]) -> None:
+        # TODO: Remove
         self._enable_ui()
         if succesful:
             netcdf_path = Path(results[FmiEnfuserLoaderAlg.OUTPUT])
