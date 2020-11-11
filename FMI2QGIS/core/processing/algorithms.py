9d2168fc655ea19fc7294eb9e2015923768189b2
Joonalai <joona@gispo.fi>
2020-11-11 09:18:39 +0200

    #8: Add copyright text

--- FMI2QGIS/core/processing/algorithms.py
+++ FMI2QGIS/core/processing/algorithms.py
@@ -1,3 +1,22 @@
+#  Gispo Ltd., hereby disclaims all copyright interest in the program FMI2QGIS
+#  Copyright (C) 2020 Gispo Ltd (https://www.gispo.fi/).
+#
+#
+#  This file is part of FMI2QGIS.
+#
+#  FMI2QGIS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  FMI2QGIS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with FMI2QGIS.  If not, see <https://www.gnu.org/licenses/>.
+
 import datetime
 from pathlib import Path
 from typing import Dict, Any, Set
